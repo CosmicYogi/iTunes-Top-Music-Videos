d97044c68d4106dff46cce3c0ae0d530e76a0124
Mitesh Soni <mitesh_soni@rocketmail.com>
2016-09-17 18:47:26 +0530

    Extracting JSON working

--- MusicVideo/APIManager.swift
+++ MusicVideo/APIManager.swift
@@ -16,14 +16,30 @@ class APIManager{
 
         let url = NSURL(string: urlString);
         let task = session.dataTaskWithURL(url!) { (data, response, error) in
-            
-            dispatch_async(dispatch_get_main_queue()){
-                if error != nil{
+            //If I am not wrong here we had used dispatch_adync(dispatch_get_main_queue()) because the session is running in background and when we are using stuff of this closure then we are putting this back to main thread and then doing it.
+
+            if error != nil{
+                dispatch_async(dispatch_get_main_queue(), { 
                     print("error aa gayi hai beta");
                     completion(result: error!.localizedDescription);
-                } else{
-                    completion(result: "NSURLSession successful");
-                    print(data?.description);
+                })
+            } else{
+                //JSON Serialization works here
+                //print(data?.description);
+                do{
+                    if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? [String : AnyObject]{
+                        print(json);
+                        let priority = DISPATCH_QUEUE_PRIORITY_HIGH;
+                        dispatch_async(dispatch_get_global_queue(priority, 0), { 
+                            dispatch_async(dispatch_get_main_queue(), { 
+                                completion(result: "JSON Serialization successful");
+                            })
+                        })
+                    }
+                } catch{
+                    dispatch_async(dispatch_get_main_queue(), { 
+                        completion(result: "Error in NSJSONSerialization");
+                    })
                 }
             }
 
