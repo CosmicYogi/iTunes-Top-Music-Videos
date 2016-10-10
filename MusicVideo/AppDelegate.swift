56f75dadd0bcb44c4bf55f5da5950d0ebb95f688
Mitesh Soni <mitesh_soni@rocketmail.com>
2016-10-10 13:32:34 +0530

    Added reachability but not working

--- MusicVideo/AppDelegate.swift
+++ MusicVideo/AppDelegate.swift
@@ -7,38 +7,63 @@
 //
 
 import UIKit
+var reachability : Reachability?
+var reachabilityStatus = " ";
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var internetCheck : Reachability?;
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        print("in didFinishLaunchingWithOptions");
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "reachabilityChanged:", name: kReachabilityChangedNotification, object: nil);
+        internetCheck = Reachability.reachabilityForInternetConnection();
+        internetCheck?.startNotifier();
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    func applicationWillResignActive(application: UIApplication) {
+        print("application will resign active");
     }
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    func reachabilityChanged(notification : NSNotification){
+        print("reachability changed");
+        reachability = notification.object as? Reachability;
+        statusChangedWithReachability(reachability!);
+    }
+    
+    func statusChangedWithReachability(currentRachabilityStatus: Reachability){
+        let networkStatus: NetworkStatus = currentRachabilityStatus.currentReachabilityStatus();
+        
+        switch networkStatus.rawValue {
+        case NotReachable.rawValue:
+            reachabilityStatus = NOACCESS;
+        case ReachableViaWiFi.rawValue:
+            reachabilityStatus = WIFI;
+        case ReachableViaWWAN.rawValue:
+            reachabilityStatus = WWAN;
+        default:
+            return;
+        }
+        NSNotificationCenter.defaultCenter().postNotificationName("ReachStatusChanged", object: nil);
+    }
+    func applicationDidEnterBackground(application: UIApplication) {
+        print("application did enter background");
     }
 
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    func applicationWillEnterForeground(application: UIApplication) {
+        print("appplication will enter foreground");
     }
 
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    func applicationDidBecomeActive(application: UIApplication) {
+        print("application did become active");
     }
 
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    func applicationWillTerminate(application: UIApplication) {
+        print("application will terminate");
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: kReachabilityChangedNotification, object: nil);
     }
 
 
--- MusicVideo/ViewController.swift
+++ MusicVideo/ViewController.swift
@@ -10,23 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "reachabilityStatusChanged", name: "ReachStatusChanged", object: nil);
+        reachabilityStatusChanged();
         let apiManager = APIManager();
         apiManager.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json", completion: loadData);
-    }
-
-    func loadData(data: String) -> Void {
-        print(data);
-        let alert = UIAlertController(title: data, message: nil, preferredStyle: .Alert);
         
-        let okActio = UIAlertAction(title: "OK", style: .Default) { (action) in
-            
-        }
-        alert.addAction(okActio);
-        self.presentViewController(alert, animated: true, completion: nil);
     }
 
+    func loadData(videos: [Videos]) -> Void {
+        for video in videos{
+            print("name = \(video.vName)");
+        }
+    }
+
+    func reachabilityStatusChanged(){
+        print("reached reachabilityStatusChanged");
+        switch reachabilityStatus {
+        case WIFI:
+            view.backgroundColor = UIColor.greenColor();
+            displayLabel.text = "WIFI available";
+            print("Wifi");
+        case WWAN:
+            view.backgroundColor = UIColor.blueColor();
+            displayLabel.text = "Cellular internet available";
+            print("Cellular");
+        case NOACCESS:
+            view.backgroundColor = UIColor.redColor();
+            displayLabel.text = "No internet access";
+            print("No access");
+        default:
+            return;
+        }
+    }
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: "ReachStatusChanged", object: nil);
+    }
 
 }
-
