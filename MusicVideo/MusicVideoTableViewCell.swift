e94cbbf31b41e7fd0a41c3f23888459b8034e3fd
Mitesh Soni <mitesh_soni@rocketmail.com>
2016-10-15 16:33:04 +0530

    Basic UI, Data fetched visible on tableView

---
+++ MusicVideo/MusicVideoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MusicVideoTableViewCell.swift
+//  MusicVideo
+//
+//  Created by mitesh soni on 15/10/16.
+//  Copyright © 2016 Mitesh Soni. All rights reserved.
+//
+
+import UIKit
+
+class MusicVideoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
