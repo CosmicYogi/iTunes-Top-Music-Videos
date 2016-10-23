83296afc2e9a189b7d763de9dec8e1a739a67794
Mitesh Soni <mitesh_soni@rocketmail.com>
2016-10-23 09:49:07 +0530

    Added Touch-ID security to avoid unwanted sharing

--- MusicVideo/MusicVideoDetailVC.swift
+++ MusicVideo/MusicVideoDetailVC.swift
@@ -9,15 +9,17 @@
 import UIKit
 import AVKit
 import AVFoundation
+import LocalAuthentication
+
 class MusicVideoDetailVC: UIViewController {
 
     var videos : Videos!
+    var securitySwitch : Bool = false;
     @IBOutlet weak var vName: UILabel!
     @IBOutlet weak var videoImage: UIImageView!
     @IBOutlet weak var vGenre: UILabel!
     @IBOutlet weak var vRights: UILabel!
     @IBOutlet weak var vPrice: UILabel!
-    
     
     
     override func viewDidLoad() {
@@ -37,7 +39,87 @@ class MusicVideoDetailVC: UIViewController {
 
     
     @IBAction func socialMedia(_ sender: UIBarButtonItem) {
-        shareMedia();
+        securitySwitch = UserDefaults.standard.bool(forKey: "SecSetting");
+        
+        switch securitySwitch {
+        case true:
+            touchIDCheck();
+        default:
+            shareMedia();
+        }
+    }
+    
+    func touchIDCheck(){
+        let alert = UIAlertController(title: "", message: "", preferredStyle: .alert);
+        alert.addAction(UIAlertAction(title: "Continue", style: .cancel, handler: nil));
+        
+        //CREATING A LOCAL AUTHENTICATION CONTEXT
+        let context = LAContext();
+        var touchIDError : NSError?
+        let reasonString = "Touch ID authentication is needed to share info on social media";
+        
+        //CHECKING IF WE CAN ACCESS LOCAL DEVICE AUTHENTICATION
+        if (context.canEvaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, error: &touchIDError)){
+           //CHECKING WHAT WAS AUTHENTICATION RESPONSE
+            context.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: reasonString, reply: { (success, policyError) in
+                if success{
+                    //SUCCESSFUL AUTHENTICATION OF USER VIA BIOMETRICS
+                    let priority = DispatchQueue.global(qos: .userInitiated);
+                    priority.async {
+                        DispatchQueue.main.async {
+                            [unowned self] in self.shareMedia();
+                        }
+                    }
+                } else{
+                    alert.title = "UnSuccessful";
+                    
+                    switch policyError{
+                    case LAError.appCancel?:
+                       alert.message = "Authentication was cancelled by the application";
+                    case LAError.authenticationFailed?:
+                        alert.message = "The user failed to provide valid credentials";
+                    case LAError.passcodeNotSet?:
+                        alert.message = "Passcode is not set on the device";
+                    case LAError.systemCancel?:
+                        alert.message = "Authentication was cancelled by the system";
+                    case LAError.touchIDLockout?:
+                        alert.message = "Too many failed attempts";
+                    case LAError.userCancel?:
+                        alert.message = "You cancelled the request";
+                    case LAError.userFallback?:
+                        alert.message = "Password not accepted, Must use Touch-ID";
+                    default:
+                        alert.message = "Unable to authenticate";
+                    }
+                    DispatchQueue.global(qos: .userInitiated).async {
+                        DispatchQueue.main.async {
+                            self.present(alert, animated: true, completion: nil);
+                        }
+                    }
+                }
+            })
+        } else{
+            //UNABLE TO ACCESS DEVICE LOCAL AUTHENTICATION
+            alert.title = "Error";
+            
+            switch touchIDError {
+            case LAError.touchIDNotEnrolled?:
+                alert.message = "Touch-ID is not enrolled";
+            case LAError.touchIDNotAvailable?:
+                alert.message = "Touch-ID is not availabe on device";
+            case LAError.passcodeNotSet?:
+                alert.message = "Passcode had not been set";
+            case LAError.invalidContext?:
+                alert.message = "The context is invalid";
+            default:
+                alert.message = "Local authentication is not available";
+            }
+            DispatchQueue.global(qos: .userInitiated).async {
+                [unowned self] in
+                self.present(alert, animated: true, completion: nil);
+            }
+        }
+        
     }
     
     func shareMedia(){
